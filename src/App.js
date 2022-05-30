6141f5d605c1a160cdd35d3ac2169c4692505476
nawresgh <nawresgressi95@gmail.com>
2022-05-30 23:51:25 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { React ,useState } from 'react';
 
+import './App.css';
+import { movieList } from './data';
+import iaamge from "./Component/thumb-1920-984394.jpg"
+
+import AddMovie  from './Component/AddMovie/AddMovie';
+import Filter from './Component/Filter/Filter';
+import  MovieList from './Component/MovieList/MovieList';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [moviesM,setMoviesM] = useState(movieList)
+
+const getNewMovie =(newM) =>{setMoviesM([...moviesM,{...newM,id:moviesM.length}])}
+const [filterByName,setFilterByName]=useState("")
+const [filterByRate ,setFilterByRate] = useState(0)
+
+return (
+    <div  className="App" style={{ 
+      backgroundImage: `url("https://ec.europa.eu/research-and-innovation/sites/default/files/styles/w1108/public/hm/field/image/milky-way-984050_1280.jpg?itok=CN6lNq-3")`
+    }}>
+      <div className='nav' style={{background:" #92FE9D"}}>
+      <Filter  setFilterByName={setFilterByName}  setFilterByRate={setFilterByRate}/></div>
+      
+      <MovieList  movieList={moviesM.filter((el)=>el.title.toLowerCase().includes(filterByName.trim().toLowerCase())&&el.rating>=filterByRate)}/>
+      <AddMovie getNewMovie={getNewMovie}/>
+     </div>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const movieList = [
+  {
+    title: "Sonic the Hedgehog 2",
+    posterurl:
+      "https://image.tmdb.org/t/p/w1280/6DrHO1jr3qVrViUO6s6kFiAGM7.jpg",
+    rating: 8.2,
+    description:
+      "After settling in Green Hills, Sonic is eager to prove he has what it takes to be a true hero. ",
+    id: 0,
+  },
+  {
+    title: "The Bad Guys",
+    posterurl:
+      "https://image.tmdb.org/t/p/w1280/7qop80YfuO0BwJa1uXk1DXUUEwv.jpg",
+    rating: 8.6,
+    description:
+      "When the infamous Bad Guys are finally caught after years of countless heists and being the world’s most-wanted villains",
+    id: 1,
+  },
+  {
+    title: "Spider-Man: No Way Home",
+    posterurl:
+      "https://image.tmdb.org/t/p/w1280/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg",
+    rating: 9.5,
+    description:
+      "Peter Parker is unmasked and no longer able to separate his normal life from the high-stakes of being a super-hero. ",
+    id: 2,
+  },
+  {
+    title: "Ambulance",
+    posterurl:
+      "https://image.tmdb.org/t/p/w1280/kuxjMVuc3VTD7p42TZpJNsSrM1V.jpg",
+    rating: 9.2,
+    description:
+      "Decorated veteran Will Sharp, desperate for money to cover his wife's medical bills",
+    id: 3,
+  },
+];
